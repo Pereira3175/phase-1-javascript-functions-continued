2da1414ad403795267d4ce261e1a026055e58a44
Luis <pereira1214@gmail.com>
2021-08-25 17:29:51 -0400

    commit tree 37f7d637aab8bb5d960769d0b064a0e09939a22e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return  `This Saturday, I want to ${activity}!`;
+}
+let mondayWork = function work(workActivity = 'go to the office'){
+ 
+    return `This Monday, I will ${workActivity}.`
+}
+
+let dayOne = mondayWork();
+
+function wrapAdjective (symbol="*") {
+    let emphatic = 'a dedicated programer'
+    const specialName = function(comment="special") {
+    return `You are ${symbol}${comment}${symbol}!`
+}
+    return specialName;
+}
